feaf8fc3f14f4f30958d7638a0c3d4371cd7ff75
Kiskenbassker <kiskenbassker@gmail.com>
2017-03-01 01:45:55 +0200

    Adding binary search.

--- search.php
+++ search.php
@@ -27,5 +27,46 @@
             return 'Please enter number from 1 to 900 and array!';
         }
     }
-    straightSearch(899, $rn);
+    //straightSearch(899, $rn);
+
+/**
+ * Функция бинарного поиска по упорядоченному массиву
+ * @param array $source_array Упорядоченный по возрастанию массив целых чисел
+ * @param int $search_value Искомая величина
+ * @return bool|int
+ */
+function binarySearch(&$source_array, $search_value)
+{
+    sort($source_array);
+    $count = count($source_array); // считаем количество элементов в массиве
+    if ($count < pow(2, 31) && is_int($search_value)) {
+        $start = 0; // индекс первого элемента массива
+        $end = $count - 1; // индекс последнего элемента массива
+        while (true) {
+            $len = $end - $start; // длина массива
+            if ($len > 2) {
+                if ($len % 2 != 0) $len++;
+                $mid = (int)($len / 2 + $start);
+            } elseif ($len >= 0) {
+                $mid = $start;
+            } else {
+                return false;
+            }
+            if ($source_array[$mid] == $search_value) {
+                while (($mid != 0) && ($source_array[$mid - 1] == $search_value))
+                    $mid--;
+                echo $mid;
+                return $mid;
+            } elseif ($source_array[$mid] > $search_value) {
+                $end = $mid - 1;
+            } else {
+                $start = $mid + 1;
+            }
+        }
+    } else {
+        return false;
+    }
+}
+
+binarySearch($rn, 345);
 ?>
