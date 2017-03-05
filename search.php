1d7108012c6779f1b9c322f292ff81784cbe34c8
Kiskenbassker <kiskenbassker@gmail.com>
2017-03-06 01:17:02 +0200

    Adding RIGHT search speed test through array.

--- search.php
+++ search.php
@@ -1,7 +1,6 @@
 <?php
 
     $rn = range(1, 900);
-    shuffle($rn);
 /**
  * Straight search through array
  *
@@ -10,17 +9,14 @@
  * @return int|mixed|string
  */
     function straightSearch($num, $numarray) {
-        $start = $_SERVER['REQUEST_TIME'];
-        if((is_array($numarray))&&(is_numeric($num))&&($num > 0)&&($num < 901)) {
+        if((is_array($numarray))&&(is_numeric($num))&&($num > 0)&&($num <= max($numarray))) {
+            $indexator = 0;
             foreach ($numarray as $integer) {
-                $indexator = 0;
                 if($num === $integer) {
                     $indexator = array_search($integer, $numarray);
                     break;
                 }
             }
-            $alltime = microtime(true) - $start;
-            echo 'Your integer has been found by Straight search on position '.$indexator.' for '.$alltime. ' seconds';
             return $indexator;
         } else {
             echo 'Please enter number from 1 to 900 and array!';
@@ -29,15 +25,13 @@
     }
 
 /**
- * Функция бинарного поиска по упорядоченному массиву
- * @param array $source_array Упорядоченный по возрастанию массив целых чисел
- * @param int $search_value Искомая величина
+ * Binary search through sorted array
+ * @param array $source_array Sorted integers array
+ * @param int $search_value Integer to find
  * @return bool|int
  */
-function binarySearch(&$source_array, $search_value)
+function binarySearch($search_value, &$source_array)
 {
-    $start_time = $_SERVER['REQUEST_TIME'];
-    sort($source_array);
     $count = count($source_array); // считаем количество элементов в массиве
     if ($count < pow(2, 31) && is_int($search_value)) {
         $start = 0; // индекс первого элемента массива
@@ -55,8 +49,6 @@ function binarySearch(&$source_array, $search_value)
             if ($source_array[$mid] == $search_value) {
                 while (($mid != 0) && ($source_array[$mid - 1] == $search_value))
                     $mid--;
-                $alltime = microtime(true) - $start_time;
-                echo 'Binary search had found your integer on position ' .$mid. ' in '.$alltime.' seconds';
                 return $mid;
             } elseif ($source_array[$mid] > $search_value) {
                 $end = $mid - 1;
@@ -69,6 +61,21 @@ function binarySearch(&$source_array, $search_value)
     }
 }
 
-straightSearch(899, $rn);
-binarySearch($rn, 345);
+// Loops for testing speed of straight and binary search
+$wholetime1 = 0;
+for($i=0; $i<=100000; $i++) {
+    $start_time1 = microtime(true);
+    straightSearch(899, $rn);
+    $alltime1 = microtime(true) - $start_time1;
+    $wholetime1 = $wholetime1 + $alltime1;
+}
+$wholetime2 = 0;
+for($j=0; $j<=100000; $j++) {
+    $start_time2 = microtime(true);
+    binarySearch(899, $rn);
+    $alltime2 = microtime(true) - $start_time2;
+    $wholetime2 = $wholetime2 + $alltime2;
+}
+echo 'The Straight Search has been finished in '.$wholetime1.' seconds'."\r\n";
+echo 'The Binary Search has been finished in '.$wholetime2.' seconds'."\r\n";
 ?>
